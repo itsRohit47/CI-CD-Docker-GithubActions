27461d7921ccdf47f0340290ef792d4bbbfea246
Rohit Bajaj <108188800+itsRohit47@users.noreply.github.com>
2023-05-30 03:16:53 +0000

    some new updates

---
+++ test/server.test.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai");
+const request = require("supertest");
+const app = require("../server");
+
+describe("Server", () => {
+  it("should serve index.html on / route", (done) => {
+    request(app)
+      .get("/")
+      .expect(200)
+      .expect("Content-Type", "text/html; charset=UTF-8")
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.text).to.include("<html>");
+        expect(res.text).to.include("<title>Text Transformer App</title>");
+        done();
+      });
+  });
+});
